90c6924e769325453f6eab53b316f6c849806210
Alessandro Moraes <sanfatec@gmail.com>
2016-06-24 16:31:47 -0300

    Correction to show styles

--- src/main/resources/static/js/controllers/ViewController.js
+++ src/main/resources/static/js/controllers/ViewController.js
@@ -4,15 +4,20 @@
  */
 var app = angular.module('fakewebmail.controllers');
 
-app.controller('ViewController', function($scope, $rootScope, $location, $routeParams, EmailsFactory, $sanitize){
+app.controller('ViewController', function($scope, $rootScope, $log, $location, $routeParams, EmailsFactory, $sanitize, $sce){
+	
+	$scope.htmlBody = null;
+	
 	$scope.email = EmailsFactory.get({id:$routeParams.id}, function(){
 		$scope.showHtml = true;
 		if($scope.email.htmlBody == null || $scope.email.htmlBody == ''){
 			$scope.showHtml = false;
+		}else{
+			$scope.htmlBody = $sce.trustAsHtml($scope.email.htmlBody);
 		}	
 		$scope.showAttachments = false;
 		if($scope.email.attachments != null && $scope.email.attachments.length > 0){
-			$scope.showAttachments = true;	
+			$scope.showAttachments = true;
 		}
 		$scope.email.$update();
 	}); 
